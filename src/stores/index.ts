4e50f4f6e4b5f020b345109d8ebd21f1a2129b94
Lee-Seungje <moondgod@gmail.com>
2024-02-18 01:22:30 +0900

    Add type

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,4 @@
 export { default as useImageState } from "./useImageStore";
 export { default as useSolveState } from "./useSolveStore";
 export { default as useEngSolveState } from "./useEngSolveStore";
+export { default as useTypeState } from "./useTypeStore";
