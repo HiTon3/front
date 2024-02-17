0c5477bcc20c6091c38fc27f3e0b51f2b07d6584
Lee-Seungje <moondgod@gmail.com>
2024-02-17 21:22:15 +0900

    Add loginPage

---
+++ src/components/Input/style.ts
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const InputTitle = styled.span`
+  color: ${({ theme }) => theme.color.grey["90"]};
+
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+
+  padding-left: 4px;
+`;
+
+export const Input = styled.input`
+  padding: 16px;
+
+  border-radius: 12px;
+  border: 1px solid ${({ theme }) => theme.color.grey["30"]};
+  background: ${({ theme }) => theme.color.grey["20"]};
+
+  color: ${({ theme }) => theme.color.grey["50"]};
+
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+`;
---
+++ src/pageContainer/LoginPage/index.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { VectorIcon } from "@/assets";
+import { Input } from "@/components";
+import * as S from "./style";
+
+import { useState } from "react";
+
+import { useRouter } from "next/navigation";
+
+const Setting = () => {
+  const [email, setEmail] = useState<string>("");
+  const [pw, setPw] = useState<string>("");
+  const [checkPw, setCheckPw] = useState<string>("");
+  const [name, setName] = useState<string>("");
+
+  const { push } = useRouter();
+
+  const inputArr = [
+    {
+      inputValue: email,
+      setInputValue: setEmail,
+      type: "email",
+      title: "이메일",
+      placeHolder: "example@ex.com",
+    },
+    {
+      inputValue: pw,
+      setInputValue: setPw,
+      type: "password",
+      title: "비밀번호",
+      placeHolder: "비밀번호를 입력해주세요",
+    },
+    {
+      inputValue: checkPw,
+      setInputValue: setCheckPw,
+      type: "password",
+      title: "비밀번호 재입력",
+      placeHolder: "비밀번호를 다시 입력해주세요",
+    },
+    {
+      inputValue: name,
+      setInputValue: setName,
+      type: "text",
+      title: "이름",
+      placeHolder: "이름을 입력해주세요",
+    },
+  ];
+
+  return (
+    <S.Layout>
+      <S.GoBack onClick={() => push("/prelogin")}>
+        <VectorIcon />
+      </S.GoBack>
+      <S.Title>아래 정보를 입력해주세요</S.Title>
+
+      {inputArr.map((input) => (
+        <Input
+          title={input.title}
+          setInputValue={input.setInputValue}
+          inputValue={input.inputValue}
+          placeHolder={input.placeHolder}
+          type={input.type as "email" | "password" | "text"}
+        />
+      ))}
+
+      <S.Button>확인</S.Button>
+    </S.Layout>
+  );
+};
+
+export default Setting;
